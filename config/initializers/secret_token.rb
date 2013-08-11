39056083214cd878dff2ed0f00a6eeb267b7fdd2
Brian Devins <badevins@gmail.com>
2013-08-11 14:20:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsHockeyStats::Application.config.secret_key_base = '38820d14f5586589402b2ef2dfd502c4185d717c8fa113c20c288e537c7701f535ff4de71eb3d673e9781f4ff2d65bca5dcbd4a0c6b1631df73c0a5642eac782'
